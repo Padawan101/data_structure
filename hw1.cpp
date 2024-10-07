2f359c37f1d0bb1281597ae4e2465f1146822bee
Padawan <ben999261@gmail.com>
2024-10-08 02:10:55 +0800

    test

---
+++ hw1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+using namespace std;
+
+typedef struct triple{
+		int row;
+		int col;
+		double value;
+} Triple;
+typedef vector<Triple> sparseMatrix;
+
+int main(){
+
+
+		return 0;
+}
